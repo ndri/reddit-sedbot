5242ec8a5f97fd897ec4dff4152eca4fb9331103
Andri Soone <andri@tfwno.gf>
2017-12-07 19:01:11 +0200

    Update reddit-sedbot.py

--- reddit-sedbot.py
+++ reddit-sedbot.py
@@ -12,35 +12,40 @@ with open("blacklist.json") as f:
 for sub in blacklist["disallowed"] + blacklist["permission"]:
     subreddit.filters.add(sub)
 
-for comment in subreddit.stream.comments():
-    text = comment.body
+while 1:
+    try:
+        for comment in subreddit.stream.comments():
+            text = comment.body
 
-    if text.lstrip("`").lstrip().startswith("sed"):
-        matches = re.findall(regex, text)
+            if text.lstrip("`").lstrip().startswith("sed"):
+                matches = re.findall(regex, text)
 
-        if matches:
-            try:
-                parent = comment.parent().body.replace(footer, "")
-            except:
-                print("Match but no parent: " + comment.permalink())
-                continue
+                if matches:
+                    try:
+                        parent = comment.parent().body.replace(footer, "")
+                    except:
+                        print("Match but no parent: " + comment.permalink)
+                        continue
 
-            command = ["sed"]
+                    command = ["sed"]
 
-            for match in matches:
-                command += ["-e", "s/{}/{}/{}".format(match[0], match[1], match[2])]
+                    for match in matches:
+                        command += ["-e", "s/{}/{}/{}".format(match[0], match[1], match[2])]
 
-            echo = Popen(["echo", "-n", parent], stdout = PIPE)
-            sed = Popen(command, stdin = echo.stdout, stdout = PIPE)
-            reply = sed.stdout.read().decode("utf-8")
+                    echo = Popen(["echo", "-n", parent], stdout = PIPE)
+                    sed = Popen(command, stdin = echo.stdout, stdout = PIPE)
+                    reply = sed.stdout.read().decode("utf-8")
 
-            print()
-            print(comment.permalink())
-            print(parent)
-            print(text)
-            print(reply)
+                    print()
+                    print(comment.permalink)
+                    print(parent)
+                    print(text)
+                    print(reply)
 
-            if reply != parent:
-                if footer not in reply:
-                    reply += footer
-                comment.reply(reply)
+                    if reply != parent and comment.author != reddit.user.me():
+                        if footer not in reply:
+                            reply += footer
+                        comment.reply(reply)
+
+    except Exception as error:
+        print(error)
