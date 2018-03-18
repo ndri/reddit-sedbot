ac8f247426734e3d5902cc2627c92f9f1b1e467b
Gil Sousa <tiagosousafl@gmail.com>
2018-03-18 10:18:21 +0000

    Move main code into a function

--- reddit-sedbot.py
+++ reddit-sedbot.py
@@ -4,48 +4,54 @@ from subprocess import Popen, PIPE
 regex = r"(?:\s+|^|'|\")s\/((?:[^/\\]|\\.)+)\/((?:[^/\\]|\\.)*)\/([0-9gI]*)"
 footer = "\n\n---\n^^reddit ^^sedbot ^^| ^^[info](https://github.com/ndri/reddit-sedbot)"
 
-reddit = praw.Reddit("reddit-sedbot")
-subreddit = reddit.subreddit("all")
 
-with open("blacklist.json") as f:
-    blacklist = json.loads(f.read())
-for sub in blacklist["disallowed"] + blacklist["permission"]:
-    subreddit.filters.add(sub)
+def main():
+    reddit = praw.Reddit("reddit-sedbot")
+    subreddit = reddit.subreddit("all")
 
-while 1:
-    try:
-        for comment in subreddit.stream.comments():
-            text = comment.body
+    with open("blacklist.json") as f:
+        blacklist = json.loads(f.read())
+    for sub in blacklist["disallowed"] + blacklist["permission"]:
+        subreddit.filters.add(sub)
 
-            if text.lstrip("`").lstrip().startswith("sed"):
-                matches = re.findall(regex, text)
+    while 1:
+        try:
+            for comment in subreddit.stream.comments():
+                text = comment.body
 
-                if matches:
-                    try:
-                        parent = comment.parent().body.replace(footer, "")
-                    except:
-                        print("Match but no parent: " + comment.permalink)
-                        continue
+                if text.lstrip("`").lstrip().startswith("sed"):
+                    matches = re.findall(regex, text)
 
-                    command = ["sed"]
+                    if matches:
+                        try:
+                            parent = comment.parent().body.replace(footer, "")
+                        except:
+                            print("Match but no parent: " + comment.permalink)
+                            continue
 
-                    for match in matches:
-                        command += ["-e", "s/{}/{}/{}".format(match[0], match[1], match[2])]
+                        command = ["sed"]
 
-                    echo = Popen(["echo", "-n", parent], stdout = PIPE)
-                    sed = Popen(command, stdin = echo.stdout, stdout = PIPE)
-                    reply = sed.stdout.read().decode("utf-8")
+                        for match in matches:
+                            command += ["-e", "s/{}/{}/{}".format(match[0], match[1], match[2])]
 
-                    print()
-                    print(comment.permalink)
-                    print(parent)
-                    print(text)
-                    print(reply)
+                        echo = Popen(["echo", "-n", parent], stdout = PIPE)
+                        sed = Popen(command, stdin = echo.stdout, stdout = PIPE)
+                        reply = sed.stdout.read().decode("utf-8")
 
-                    if reply != parent and comment.author != reddit.user.me():
-                        if footer not in reply:
-                            reply += footer
-                        comment.reply(reply)
+                        print()
+                        print(comment.permalink)
+                        print(parent)
+                        print(text)
+                        print(reply)
 
-    except Exception as error:
-        print(error)
+                        if reply != parent and comment.author != reddit.user.me():
+                            if footer not in reply:
+                                reply += footer
+                            comment.reply(reply)
+
+        except Exception as error:
+            print(error)
+
+
+if __name__ == '__main__':
+    main()
